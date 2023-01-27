5435810cd7cfd5687c05d69784f355e2fee471fe
getekj <joannagetek@gmail.com>
2023-01-27 14:38:09 -0500

    added page outlines and add book button

---
+++ bookstore_frontend/src/components/AddBookButton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function AddBookButton() {
+    return (
+      <button>Add Book</button>
+    );
+  }
+
+export default AddBookButton;
--- bookstore_frontend/src/pages/AddBookPage.js
+++ bookstore_frontend/src/pages/AddBookPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import AddBookButton from '../components/AddBookButton';
 
 
 function AddBookPage () {
     return (
         <>
         <h1> HELLO THIS IS AddBookPage </h1>
+        <AddBookButton/>
         </>
 
     );
