acfeeb6113db9ace54570072c7b21ddeed98602b
getekj <joannagetek@gmail.com>
2023-01-28 18:39:54 -0500

    adding new update catalog page

---
+++ bookstore_frontend/src/components/UpdateCatalogButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function UpdateCatalogButton() {
+    return (
+    <nav className='update_catalog_button'>
+        <a href="/addorupdatebook">Update Catalog</a>
+    </nav>
+    );
+  }
+
+export default UpdateCatalogButton;
---
+++ bookstore_frontend/src/pages/AddOrUpdatePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AddOrUpdatePage () {
+    return (
+        <>
+        <h1>ADD OR UPDATE PAGE</h1>
+        </>
+    );
+};
+
+export default AddOrUpdatePage;
