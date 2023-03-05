555b4c0c2ed01c895823ad1367a2baf096338581
getekj <joannagetek@gmail.com>
2023-03-04 20:42:56 -0500

    fixed homepage formatting and starting on serach bar

---
+++ bookstore_frontend/src/components/SearchBar.js
@@ -0,0 +1,16 @@
+
+function SearchBar () {
+    return (
+        <div className="search-bar-wrapper">
+            <div className="search-field">
+                <input
+                    type="text"
+                    placeholder="Enter Book Title Here ... "
+                ></input>
+            </div>
+
+        </div>
+    )
+};
+
+export default SearchBar;
--- bookstore_frontend/src/pages/HomePage.js
+++ bookstore_frontend/src/pages/HomePage.js
@@ -5,21 +5,28 @@ import ActivityButton from '../components/ActivityButton';
 import ViewTableButton from '../components/ViewTableButton';
 import "./HomePage.css";
 import Annhilation from '../components/Annhilation.png';
+import SearchBar from '../components/SearchBar';
 
 function HomePage () {
     return (
         <>
         <h1 className='bookstore_title'> Logos BookStore </h1>
-        <div className='featured'>
-            <p> New Books Featured This Week </p>
-            <img src={Annhilation} alt="Annhilation Book Cover"></img>
+
+        <div className='buttons-list'>
+            <HomeButton/>
+            <ViewTableButton/>
+            <UpdateCatalogButton/>
+            <ActivityButton/>
         </div>
 
+        <div className='featured'>
+            <div className='featured-text-wrapper'>
+                <p> New Books Featured This Week </p>
+            </div>
+            <img src={Annhilation} alt="Annhilation Book Cover"></img>
+            <SearchBar/>
+        </div>
 
-        <HomeButton/>
-        <UpdateCatalogButton/>
-        <ActivityButton/>
-        <ViewTableButton/>
         </>
 
     );
